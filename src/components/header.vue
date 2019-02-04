73d2ab3fbd9e3ffe012de9eab3b06e26e932c378
k.sunaga-cq <k.sunaga@caraquri.com>
2019-02-04 22:15:40 +0900

    ヘッダーのメニュー作成

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,15 @@
 <template>
-  <header class="sticky-top">
-    <div class="title">kSunaga's portfolio</div>
-    <div class="col-8"></div>
+  <header class="sticky-top row">
+    <div class="col-5"></div>
+    <div class="title col-3">
+      <p>kSunaga's portfolio</p>
+    </div>
+    <div class="link col-4">
+      <p>Home</p>
+      <p>Skills</p>
+      <p>Products</p>
+      <p>Contact</p>
+    </div>
   </header>
 </template>
 
@@ -10,11 +18,16 @@
     line-height: 4;
     border-bottom: solid 2px lightblue;
     background-color: white;
-  }
-  .title {
     border-top: solid 3px #85ce9e;
     font-family: "Comic Sans MS";
     font-size: larger;
     text-align: center;
+  }
+  .title {
+    text-align: left;
+  }
+  .link p{
+    padding-left: 15px;
+    display: inline-block;
   }
 </style>
